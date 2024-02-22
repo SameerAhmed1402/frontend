56fc1a07d481e3bde0e3f39a6b980200430afa14
SameerAhmed1402 <sameerkuraishi1402@gmail.com>
2024-02-22 23:08:41 +0530

    initial

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1 from './product_1.png'
+import p2 from './product_2.png'
+import p3 from './product_3.png'
+import p4 from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p1,
+    new_price:50.00,
+    old_price:80.50,
+  },
+  {id:2,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p2,
+    new_price:85.00,
+    old_price:120.50,
+  },
+  {id:3,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p3,
+    new_price:60.00,
+    old_price:100.50,
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
